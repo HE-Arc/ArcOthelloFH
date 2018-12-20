6ab4097711a1953ebf13e508c113cf1910412874
Asmoday4949 <malik.fleury@he-arc.ch>
2018-12-20 09:00:33 +0100

    Add project + simple grid

---
+++ UI/OthelloGrid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Othello.UI
+{
+    class OthelloGrid: Grid
+    {
+        private int rows;
+        private int columns;
+
+        public OthelloGrid(int rows, int columns): base()
+        {
+            this.rows = rows;
+            this.columns = columns;
+
+            this.prepareGeometry();
+        }
+
+        public void prepareGeometry()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                RowDefinition rowDef = new RowDefinition();
+                this.RowDefinitions.Add(rowDef);
+            }
+
+            for (int column = 0; column < columns; column++)
+            {
+                ColumnDefinition columnDef = new ColumnDefinition();
+                this.ColumnDefinitions.Add(columnDef);
+            }
+
+            for(int row = 0;row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    Button button = new Button();
+
+                    this.Children.Add(button);
+                    Grid.SetRow(button, row);
+                    Grid.SetColumn(button, column);
+                }
+            }
+        }
+    }
+}
