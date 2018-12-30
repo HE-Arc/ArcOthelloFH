37fa3072e352ff76f1667eb0c33e8862d079bc60
malik.fleury@he-arc.ch <malik.fleury@he-arc.ch>
2018-12-30 12:15:42 +0100

    Add IA class

---
+++ IA/IA.cs
@@ -0,0 +1,57 @@
+﻿using Othello.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello.IA
+{
+    /// <summary>
+    /// WTF ?
+    /// </summary>
+    class IA : IPlayable.IPlayable
+    {
+        private OthelloLogic logic;
+
+        public IA(OthelloLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        public int GetBlackScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[,] GetBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWhiteScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPlayable(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayMove(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
