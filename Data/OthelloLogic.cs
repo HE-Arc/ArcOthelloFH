31355d8db456d0c0bfdcf47d414bc31c724bfcf0
malik.fleury@he-arc.ch <malik.fleury@he-arc.ch>
2018-12-30 12:16:37 +0100

    Add function to get players data

--- Data/OthelloLogic.cs
+++ Data/OthelloLogic.cs
@@ -14,16 +14,30 @@ namespace Othello.Data
 
         private PlayerData[] playerData;
         private int[,] gameBoard;
+        private PlayerColor playerColorTurn;
 
         public OthelloLogic(int rows = 7, int columns = 9, int initialPawnRow = 3, int initialPawnColumn = 3)
         {
-            playerData = new PlayerData[NUMBER_OF_PLAYERS];
-            gameBoard = new int[rows, columns];
-
-            InitGameBoard();
+            InitAll(rows, columns, initialPawnRow, initialPawnColumn);
         }
 
-        public void InitGameBoard(int initialPawnRow = 3, int initialPawnColumn = 3)
+        public void InitAll(int rows, int columns, int initialPawnRow, int initialPawnColumn)
+        {
+            playerData = new PlayerData[NUMBER_OF_PLAYERS];
+            gameBoard = new int[rows, columns];
+            playerColorTurn = PlayerColor.White;
+
+            InitPlayersData();
+            InitGameBoard(initialPawnRow, initialPawnColumn);
+        }
+
+        public void InitPlayersData()
+        {
+            playerData[(int)PlayerColor.Black] = new PlayerData(PlayerColor.Black);
+            playerData[(int)PlayerColor.White] = new PlayerData(PlayerColor.White);
+        }
+
+        public void InitGameBoard(int initialPawnRow, int initialPawnColumn)
         {
             for (int row = 0; row < Rows; row++)
             {
@@ -37,6 +51,21 @@ namespace Othello.Data
             gameBoard[initialPawnRow + 1, initialPawnColumn] = (int)SlotContent.Black;
             gameBoard[initialPawnRow, initialPawnColumn + 1] = (int)SlotContent.Black;
             gameBoard[initialPawnRow + 1, initialPawnColumn + 1] = (int)SlotContent.White;
+        }
+
+        public PlayerData GetPlayerDataFromColor(PlayerColor color)
+        {
+            return playerData[(int)color];
+        }
+
+        public PlayerData GetWhitePlayerData()
+        {
+            return GetPlayerDataFromColor(PlayerColor.White);
+        }
+
+        public PlayerData GetBlackPlayerData()
+        {
+            return GetPlayerDataFromColor(PlayerColor.Black);
         }
 
         public int Rows
