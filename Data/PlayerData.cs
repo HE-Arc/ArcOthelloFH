410c11da0e4074e1314dbf01a36976c6ccc17fe4
malik.fleury@he-arc.ch <malik.fleury@he-arc.ch>
2018-12-30 11:30:40 +0100

    Add data

--- Data/PlayerData.cs
+++ Data/PlayerData.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Othello.Data
 {
+    enum PlayerColor { White = 0, Black = 1 };
+
     class PlayerData
     {
+        private int numberOfPawns = 0;
+        private int secondsElapsed = 0;
+        private PlayerColor color;
+
+        public PlayerData(PlayerColor color)
+        {
+            this.color = color;
+        }
+
+        public int NumberOfPawns
+        {
+            get { return numberOfPawns; }
+            set { numberOfPawns = value; }
+        }
+
+        public int SecondsElapsed
+        {
+            get { return secondsElapsed ; }
+            set { secondsElapsed = value; } 
+        }
+
+        public PlayerColor Color
+        {
+            get { return color ;}
+        }
     }
 }
