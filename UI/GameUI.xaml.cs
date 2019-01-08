4dc96d95f971e9f9c1734727b8c46a00fbbb6ec1
malik.fleury@he-arc.ch <malik.fleury@he-arc.ch>
2019-01-08 20:27:52 +0100

    Add function to test algo for the available moves

--- UI/GameUI.xaml.cs
+++ UI/GameUI.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Othello.Data;
 using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Othello.UI
@@ -16,10 +18,39 @@ namespace Othello.UI
         {
             InitializeComponent();
 
+            // Prepare data
             logic = new OthelloLogic();
-            grid = new OthelloGrid(logic);
 
+            // Prepare the grid
+            grid = new OthelloGrid(logic);
             MainDockPanel.Children.Add(grid);
+
+            // Add the events
+            game_controls_ui.EventEndTurnPressed += endOfTurnPressed;
+
+            executeBefore();
+        }
+
+        public void executeBefore()
+        {
+            IntPosition currentPosition = new IntPosition(3, 3);
+            List<IntPosition> directionsList = logic.GetNeighborsDirections(currentPosition);
+            List<IntPosition> movements = new List<IntPosition>();
+
+            foreach (IntPosition direction in directionsList)
+            {
+                if (logic.IsPossibleMove(currentPosition, direction, movements))
+                {
+                    IntPosition possibleMove = movements[movements.Count - 1];
+                    grid.MarkSlot(possibleMove);
+                }
+                movements.Clear();
+            }
+        }
+
+        public void endOfTurnPressed(object sender, EventArgs e)
+        {
+
         }
     }
 }
